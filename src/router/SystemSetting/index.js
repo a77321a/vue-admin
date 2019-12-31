8a9ce13171201aadfac50e4e63301b2833e795bf
a77321a <1817169988@qq.com>
2019-12-31 23:27:24 +0800

    .

--- src/router/SystemSetting/index.js
+++ src/router/SystemSetting/index.js
@@ -2,14 +2,16 @@
  * @Descripttion:系统设置
  * @Author:
  * @Date: 2019-11-05 10:27:14
- * @LastEditors:
- * @LastEditTime: 2019-11-21 17:57:51
+ * @LastEditors  : Please set LastEditors
+ * @LastEditTime : 2019-12-31 23:26:44
  */
 import DictionaryManagementRoute from './DictionaryManagement'
 import RolePermissionRoute from './RolePermission'
 import SystemMenu from './SystemMenu'
 import DataSyncLog from './DataSyncLog'
-
+/**
+ * default
+ */
 export default [
   {
     path: 'SystemSetting',
